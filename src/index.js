a398d0aaae685107808aee892209a67d86ea9011
RaskiTech <raskitech@gmail.com>
2020-11-16 20:46:29 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import "bootstrap/dist/css/bootstrap.css"
 import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
 import App from "./App"
 
-
+// This is a test comment
 ReactDOM.render(<App />, document.getElementById("root"));
