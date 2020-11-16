5e6ddf10eb2ad698fb517a95d84032157ec57725
RaskiTech <raskitech@gmail.com>
2020-11-16 20:59:26 +0200

    Updates

---
+++ precache-manifest.90ac3709ccd2f7a395db98a43988aee8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c162401247a3b3327bf2d2d6918e07",
+    "url": "/find-a-vector/index.html"
+  },
+  {
+    "revision": "9f1c71ed5dc8adde8e85",
+    "url": "/find-a-vector/static/css/2.219a792f.chunk.css"
+  },
+  {
+    "revision": "9f1c71ed5dc8adde8e85",
+    "url": "/find-a-vector/static/js/2.7180af6f.chunk.js"
+  },
+  {
+    "revision": "67a6c6a45e2bb5f0a2387658d963f962",
+    "url": "/find-a-vector/static/js/2.7180af6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cfac64f7a809a4df620",
+    "url": "/find-a-vector/static/js/main.acb37634.chunk.js"
+  },
+  {
+    "revision": "56a41166db3546c9ddfa",
+    "url": "/find-a-vector/static/js/runtime-main.e35a1bc3.js"
+  }
+]);
